9c2f81717e13ee0acdd5dbb5680b9e485a5f816a
lucas-machado <lucasmach@gmail.com>
2020-01-17 01:13:11 -0300

    remove warning & made jumpForce public

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FrogController : MonoBehaviour
 {
     public float waitToJump = 5.0f;
+    public float jumpForce = 400f;
     public Rigidbody2D rigidBody;
     public Animator animator;
 
@@ -19,7 +20,7 @@ public class FrogController : MonoBehaviour
         {
             yield return new WaitForSeconds(waitToJump);
             animator.SetBool("IsJumping", true);
-            rigidBody.AddForce(new Vector2(0, 400f));
+            rigidBody.AddForce(new Vector2(0, jumpForce));
         }
     }
 
